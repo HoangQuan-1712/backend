e9d04ce4bb7394f705a3ba21120dcee212f19eb3
HoangQuan-1712 <muoinhungnhat@gmail.com>
2024-07-05 22:44:53 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
+import { UserModule } from './User/user.module';
+import { RoleModule } from './Role/role.module';
+import { Role } from './Entity/role.Entity';
+import { User } from './Entity/user.Entity';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
+    UserModule,
+    ConfigModule.forRoot(),
+    RoleModule,
+    TypeOrmModule.forFeature([Role, User]),
   ],
   controllers: [AppController],
   providers: [AppService],
